559a48b7969d25a4953caf6aff716afca4f64c1b
Evan Hirsh <evanx11@gmail.com>
2017-05-01 16:02:24 -0400

    fixed css

--- js/ehirsh.js
+++ js/ehirsh.js
@@ -2,3 +2,11 @@
 $( document ).ready(function(){ 
     $(".button-collapse").sideNav();
 })
+
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(700).fadeOut('slow');
+
+	$('.preloader-wrapper')
+		.delay(700)
+		.fadeOut();
+});
