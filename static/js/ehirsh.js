ca0c12895b42dc9562d18d0b494a619291612d7b
Evan Hirsh <evanx11@gmail.com>
2018-08-13 21:34:24 -0400

    Removed Jquery / fixed styling

--- static/js/ehirsh.js
+++ static/js/ehirsh.js
@@ -1,19 +1,6 @@
+M.AutoInit();
 
-$( document ).ready(function(){
-    $(".button-collapse").sideNav();
-    $('.parallax').parallax();
-    $(".dropdown-trigger").dropdown();
-
-})
-
-document.addEventListener("DOMContentLoaded", function(){
-
-	$('.preloader-background').delay(700).fadeOut('slow');
-
-	$('.preloader-wrapper')
-		.delay(700)
-		.fadeOut();
-
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
 });
