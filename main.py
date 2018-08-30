273e44d2fffaf54b072a83856a919223028c40de
Evan Hirsh <evanx11@gmail.com>
2018-08-30 13:35:55 -0400

    logo v1

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_view():
     return render_template('index.html')
+
 
 @app.route("/contact")
 def contact_view():
     return render_template('contact.html')
 
+
 @app.route("/pay")
 def pay_view():
     return render_template('pay.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(ssl_context='adhoc')
