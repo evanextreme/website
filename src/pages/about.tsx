346ba973652c5088bd3c8baf0c89615b393ff463
Evan Hirsh <evan@ehirsh.com>
2020-03-14 13:44:04 -0400

    move away from css, switch navbar pos

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,30 +1,13 @@
 import React from "react";
-import Image from "react-bootstrap/Image";
-import { Parallax } from "react-scroll-parallax";
-import "./about.css";
 
 const About: React.FC = () => {
   return (
-    <div>
-      {/* <Parallax
-        blur={{ min: -15, max: 15 }}
-        bgImage={}
-        bgImageAlt="the cat"
-        strength={200}
-      >
-        Hi. I'm a software engineer with years of experience in backend, full
-        stack, and distributed systems. I've worked for high valuation startups
-        and trillion dollar companies, and will be starting this fall at
-        Microsoft.
-        <div style={{ height: "1200px", width: "1200px" }} />
-      </Parallax> */}
-      <Parallax
-        className="about-description-body"
-        y={[-20, 20]}
-        tagOuter="figure"
-      >
-        <Image src={process.env.PUBLIC_URL + "img/messy_hair.jpeg"} />
-      </Parallax>
+    <div className="about-body">
+      <h3>
+        My name is Evan Hirsh. I'm software engineer with a degree in Computer
+        Science from RIT who will be starting as a full time engineer at
+        Microsoft this fall.
+      </h3>
     </div>
   );
 };
