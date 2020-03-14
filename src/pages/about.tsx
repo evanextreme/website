ba07d79c9e90944cf9677e8d43406989398d1a16
Evan Hirsh <contact@ehirsh.com>
2020-03-14 15:37:55 -0400

    Fix image / title

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import Image from "material-ui-image";
+
+import evanImage from "../assets/img/guggenheim.jpeg";
 
 const About: React.FC = () => {
   return (
     <div className="about-body">
-      <h3>
-        My name is Evan Hirsh. I'm software engineer with a degree in Computer
-        Science from RIT who will be starting as a full time engineer at
-        Microsoft this fall.
-      </h3>
+      <div className="about-body-text">
+        <h3>
+          My name is Evan Hirsh. I'm software engineer with experience in full
+          stack / distributed systems who will be working at Microsoft full time
+          this fall.
+        </h3>
+      </div>
+      <Image
+        onClick={() =>
+          console.log("Image clicked. Will do something with this later lmao")
+        }
+        src={evanImage}
+        aspectRatio={9 / 11}
+      />
     </div>
   );
 };
