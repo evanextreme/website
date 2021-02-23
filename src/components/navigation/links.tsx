b0c93e547f80aef87d24459f02500a37e857c2e4
Evan Hirsh <evan@ehirsh.com>
2021-02-23 23:51:53 +0000

    deployment / button work

---
+++ src/components/navigation/links.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const HomePageLink = (props: any) => {
+    return <Link to="/pay" {...props}></Link>;
+};
+
+export const PayPageLink = (props: any) => {
+    return <Link to="/pay" {...props}></Link>;
+};
