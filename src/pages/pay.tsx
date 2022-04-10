af6597695e38fea81913ebd0974a396ee62c0320
Evan Hirsh <contact@ehirsh.com>
2022-04-10 15:14:51 -0700

    fuck crypto

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -1,14 +1,15 @@
+import { Button, Grid } from "@material-ui/core";
 import React from "react";
 
 const Pay: React.FC = () => {
   return (
     <div className="pay-body">
       <div className="pay-body-text">
-        <h1>Crypto</h1>
-        <h3>Bitcoin: ***REMOVED***</h3>
-        <h3>Bitcoin Cash: ***REMOVED***</h3>
-        <h3>Ethereum: ***REMOVED***</h3>
-        <h3>XRP: ***REMOVED***</h3>
+      {/* <Grid container spacing={1} direction="row" justify="flex-end">
+        <Button variant="contained" href="https://www.venmo.com/evanextreme">Venmo</Button>
+      </Grid> */}
+      <h3>Venmo is <a href="https://www.venmo.com/evanextreme">@evanextreme</a></h3>
+      <h3>Message me for Apple Pay info</h3>
       </div>
     </div>
   );
