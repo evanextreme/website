e1826aeb5aa3c76ca0356422d1055f6892c27530
Evan Hirsh <contact@ehirsh.com>
2022-08-12 12:56:33 -0700

    forgot to commit this stuff
    Signed-off-by: Evan Hirsh <evan@ehirsh.com>

--- src/components/navigation/links.tsx
+++ src/components/navigation/links.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const HomePageLink = (props: any) => {
-    return <Link to="/pay" {...props}></Link>;
+  return <Link to="/about" {...props}></Link>;
 };
 
 export const PayPageLink = (props: any) => {
-    return <Link to="/pay" {...props}></Link>;
+  return <Link to="/pay" {...props}></Link>;
 };
