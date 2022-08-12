57c2158d8b927fc98b9e6a95ce1b4d03e3847a94
Evan Hirsh <evan@ehirsh.com>
2022-08-12 13:05:07 -0700

    i present exhibit c
    Signed-off-by: Evan Hirsh <evan@ehirsh.com>

--- src/pages/pay.tsx
+++ src/pages/pay.tsx
@@ -5,11 +5,13 @@ const Pay: React.FC = () => {
   return (
     <div className="pay-body">
       <div className="pay-body-text">
-      {/* <Grid container spacing={1} direction="row" justify="flex-end">
+        {/* <Grid container spacing={1} direction="row" justify="flex-end">
         <Button variant="contained" href="https://www.venmo.com/evanextreme">Venmo</Button>
       </Grid> */}
-      <h3>Venmo is <a href="https://www.venmo.com/evanextreme">@evanextreme</a></h3>
-      <h3>Message me for Apple Pay info</h3>
+        <h3>
+          Venmo is <a href="https://www.venmo.com/evanextreme">@evanextreme</a>
+        </h3>
+        <h3>Message me for Apple Pay info</h3>
       </div>
     </div>
   );
