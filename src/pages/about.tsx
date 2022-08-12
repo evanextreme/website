9a3a7a85f630eb37dd4be7270253edd85beb320c
Evan Hirsh <evan@ehirsh.com>
2022-08-12 12:56:33 -0700

    forgot to commit this stuff
    Signed-off-by: Evan Hirsh <evan@ehirsh.com>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,22 @@ const About: React.FC = () => {
     <div className="about-body">
       <div className="about-body-text">
         <h3>
-           Hey!! I'm a professional software engineer with years of experience creating full stack and distributed systems relied on by hundreds of thousands of users across the world. I graduated from RIT with a degree in computer science, and a minor in computer security. While there I also created their <a href="https://ritesports.org">esports program</a>. Right now I am making <a href="https://azure.microsoft.com/en-us/services/data-factory/">Azure Data Factory</a> / <a href="https://azure.microsoft.com/en-us/services/synapse-analytics/#overview">Synapse</a> at Microsoft.
+          Hey!! I'm a professional software engineer with years of experience
+          creating full stack and distributed systems relied on by hundreds of
+          thousands of users across the world. I graduated from RIT with a
+          degree in computer science, and a minor in computer security. While
+          there I also created their{" "}
+          <a href="https://ritesports.org">esports program</a>. Right now I am
+          making{" "}
+          <a href="https://azure.microsoft.com/en-us/services/data-factory/">
+            Azure Data Factory
+          </a>{" "}
+          /{" "}
+          <a href="https://azure.microsoft.com/en-us/services/synapse-analytics/#overview">
+            Synapse
+          </a>{" "}
+          at Microsoft. Feel free to email / tweet at me, as I respond quickly!
+          Here's a photo of me walking through the woods
         </h3>
       </div>
       <Image
